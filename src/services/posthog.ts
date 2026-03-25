3bbec6bd6f10f625df2dabf15d0036f945478efe
Dipak <dsaha9797@gmail.com>
2026-03-25 18:52:36 +0530

    posthog.ts-update

--- src/services/posthog.ts
+++ src/services/posthog.ts
@@ -2,7 +2,13 @@ import posthog from "posthog-js";
 
 posthog.init("phc_QTG3Kz6La1BL28536wugnIF3XduwvYNh6pVuhGUZN0F", {
   api_host: "https://app.posthog.com",
-  capture_pageview: false, // important
+
+  capture_pageview: false,
+  autocapture: false,
+  capture_pageleave: false,
+  capture_performance: false,
+  disable_session_recording: true,
+  disable_persistence: true,
 });
 
 export default posthog;
